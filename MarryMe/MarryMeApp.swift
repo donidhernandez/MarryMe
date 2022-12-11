1f09e501b887ca758bb27fcfdb6aafeedca05a6a
Adonai Domingez <donihernandez003@gmail.com>
2022-12-11 11:45:30 -0500

    Initial Commit

---
+++ MarryMe/MarryMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarryMeApp.swift
+//  MarryMe
+//
+//  Created by Doni on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MarryMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
