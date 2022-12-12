fd3a4557755cf39d9fb922f6769a3b099729dd78
Adonai Domingez <donihernandez003@gmail.com>
2022-12-12 16:30:06 -0500

    initial commit

--- MarryMe/MarryMeApp.swift
+++ MarryMe/MarryMeApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 @main
 struct MarryMeApp: App {
     var body: some Scene {
---
+++ MarryMe/Screens/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  MarryMe
+//
+//  Created by Doni on 12/12/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
